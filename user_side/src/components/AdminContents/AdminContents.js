0f53f5e956d5fcc59b5f63a22255f4c87a3a8d9c
ajaymadhavan14 <ajay141297@gmail.com>
2023-05-02 10:20:39 +0530

    first commit

---
+++ user_side/src/components/AdminContents/AdminContents.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './AdminContent.css'
+
+function AdminContents() {
+  return (
+    <div style={{}}>
+            <ul className="fly-in-text">
+        <li >A</li>
+        <li >D</li>
+        <li >M</li>
+        <li >I</li>
+        <li >N</li>
+        <li ></li>
+        <li ></li>
+    </ul>
+    </div>
+  )
+}
+
+export default AdminContents
